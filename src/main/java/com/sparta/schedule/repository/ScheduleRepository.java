146c26eee90822be5b38405280dad93a59f6e0ec
JIMINJI1 <mj013031@gmail.com>
2024-10-15 20:04:24 +0900

    refactor: 어노테이션 추가

--- src/main/java/com/sparta/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/sparta/schedule/repository/ScheduleRepository.java
@@ -2,11 +2,11 @@ package com.sparta.schedule.repository;
 
 import com.sparta.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-//    List<Schedule> findAll();
-//    Optional<Schedule> findById(Long ScheduleId);
+    Optional<Schedule> findById(Long ScheduleId);
 }
