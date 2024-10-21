af07a029ea857b0428b3dc1c0d10a454cbb135f4
JIMINJI1 <mj013031@gmail.com>
2024-10-21 22:48:27 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/com/sparta/schedule/repository/CommentRepository.java
+++ src/main/java/com/sparta/schedule/repository/CommentRepository.java
@@ -2,11 +2,10 @@ package com.sparta.schedule.repository;
 
 import com.sparta.schedule.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findBySchedule_ScheduleId(Long ScheduleId);
 }
--- src/main/java/com/sparta/schedule/repository/UserRepository.java
+++ src/main/java/com/sparta/schedule/repository/UserRepository.java
@@ -2,8 +2,7 @@ package com.sparta.schedule.repository;
 
 import com.sparta.schedule.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
