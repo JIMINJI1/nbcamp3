aacb8e4c291f76e59347cf29bb0cf674dd98a101
JIMINJI1 <mj013031@gmail.com>
2024-10-30 23:19:57 +0900

    feat: User 인증,인가 로직 추가

--- src/main/java/com/sparta/schedule/contorller/UserController.java
+++ src/main/java/com/sparta/schedule/contorller/UserController.java
@@ -1,13 +1,14 @@
 package com.sparta.schedule.contorller;
 
 
-import com.sparta.schedule.dto.CreateUserRequestDto;
-import com.sparta.schedule.dto.UpdateUserRequestDto;
-import com.sparta.schedule.dto.UserResponseDto;
-import com.sparta.schedule.service.ScheduleService;
+import com.sparta.schedule.dto.*;
+import com.sparta.schedule.security.UserDetailsImpl;
 import com.sparta.schedule.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,30 +17,41 @@ import org.springframework.web.bind.annotation.*;
 
 public class UserController {
     private final UserService userService;
-    private final ScheduleService scheduleService;
 
-    // 1. 유저 등록
-    @PostMapping("")
-    public UserResponseDto createUser(@Valid @RequestBody CreateUserRequestDto requestdto) {
-        return userService.createUser(requestdto);
+    // 1. 유저 등록 (회원가입)
+    @PostMapping("/signup")
+    public ResponseEntity<UserResponseDto> signup(@Valid @RequestBody CreateUserRequestDto requestdto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(requestdto));
     }
 
+
+    // 2. 유저 로그인
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto reqeustDto) {
+        return ResponseEntity.ok(userService.login(reqeustDto));
+    }
+
+
+    /* 필수구현 */
     // 2. 유저 조회
     @GetMapping("/{userId}")
-    public UserResponseDto getUser(@PathVariable Long userId) {
-        return userService.getUserById(userId);
+    public ResponseEntity<UserResponseDto> getUser(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok(userService.getUserById(userId, userDetails.getUser()));
     }
 
     // 3. 유저 수정
     @PutMapping("/{userId}")
-    public UserResponseDto updateUser(@PathVariable Long userId, @Valid @RequestBody UpdateUserRequestDto requestdto) {
-        return userService.updateUser(userId,requestdto);
+    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long userId, @Valid @RequestBody UpdateUserRequestDto requestDto,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        UserResponseDto updatedUser = userService.updateUser(userId, requestDto, userDetails.getUser());
+        return ResponseEntity.ok(updatedUser);
     }
 
     // 4. 유저 삭제
     @DeleteMapping("/{userId}")
-    public void deleteUser(@PathVariable Long userId) {
-        userService.deleteUser(userId);
-
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        userService.deleteUser(userId, userDetails.getUser());
+        return ResponseEntity.noContent().build(); // 204 No Content
     }
+
 }
--- src/main/java/com/sparta/schedule/service/UserService.java
+++ src/main/java/com/sparta/schedule/service/UserService.java
@@ -1,28 +1,59 @@
 package com.sparta.schedule.service;
 
-import com.sparta.schedule.dto.CreateUserRequestDto;
-import com.sparta.schedule.dto.UpdateUserRequestDto;
-import com.sparta.schedule.dto.UserResponseDto;
+import com.sparta.schedule.dto.*;
 import com.sparta.schedule.entity.User;
+import com.sparta.schedule.entity.UserRoleEnum;
+import com.sparta.schedule.jwt.JwtUtil;
 import com.sparta.schedule.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 
 public class UserService {
     private final UserRepository userRepository;
-    
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    // ADMIN_TOKEN
+    @Value("${admin.token}")
+    private String ADMIN_TOKEN;
+
     // 1. 유저 등록
     public UserResponseDto createUser(CreateUserRequestDto requestDto) {
+        //중복체크
+        if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("이미 사용중인 이메일입니다");
+        }
+
+        // 비밀번호 해시 처리
+        String hashPassword = hashPassword(requestDto.getPassword());
+
+        // adminToken 체크
+        if (requestDto.getAdminToken() != null && !requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+            throw new IllegalArgumentException("관리자 토큰이 일치하지 않습니다.");
+        }
+        // role 체크
+        UserRoleEnum role = Optional.ofNullable(requestDto.getAdminToken())
+                .filter(token -> token.equals(ADMIN_TOKEN))
+                .map(token -> UserRoleEnum.ADMIN)
+                .orElse(UserRoleEnum.USER);
+
         //dto -> entity
         User user = new User(
                 requestDto.getUsername(),
                 requestDto.getEmail(),
-                requestDto.getPassword()
+                hashPassword,
+                role
         );
 
         //DB 저장
@@ -34,30 +65,46 @@ public class UserService {
         return userResponseDto;
     }
 
-    // 2. 유저 조회
-    public UserResponseDto getUserById(Long userId) {
+    //2. 유저 로그인
+    public LoginResponseDto login(LoginRequestDto requestDto) {
+        // 유저 확인
+        User user = userRepository.findByEmail(requestDto.getEmail()).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치 하지 않습니다.");
+        }
+        return new LoginResponseDto(user.getUsername());
+
+    }
+
+    // 3. 유저 조회
+    public UserResponseDto getUserById(Long userId, User user) {
         // 주어진 ID로 유저 조회, 없으면 예외 발생
-        User user = validateUser(userId);
+        User existingUser = validateUser(userId);
 
         // entity -> responseDto
         return new UserResponseDto(
-           user.getUserId(),
-           user.getUsername(),
-           user.getEmail(),
-           user.getCreatedAt(),
-           user.getUpdatedAt()
+                user.getUserId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getCreatedAt(),
+                user.getUpdatedAt()
         );
     }
 
 
-    // 3. 유저 수정
+    // 4. 유저 수정
     @Transactional
-    public UserResponseDto updateUser(Long userId, UpdateUserRequestDto requestDto) {
+    public UserResponseDto updateUser(Long userId, UpdateUserRequestDto requestDto, User user) {
         // 주어진 ID로 유저 조회, 없으면 예외 발생
-        User user = validateUser(userId);
+        User existingUser = validateUser(userId);
+
+        // 비밀번호 해시 처리
+        String hashPassword = hashPassword(requestDto.getPassword());
 
         // 유저의 비밀번호 수정
-        user.updateUser(requestDto.getPassword());
+        user.updateUser(hashPassword);
 
         // 수정된 유저를 DB에 저장
         User updatedUser = userRepository.save(user);
@@ -66,12 +113,11 @@ public class UserService {
         return new UserResponseDto(updatedUser);
     }
 
-
-    // 4. 유저 삭제
+    // 5. 유저 삭제
     @Transactional
-    public void deleteUser (Long userId){
+    public void deleteUser(Long userId, User user) {
         // 주어진 ID로 유저 조회, 없으면 예외 발생
-        User user = validateUser(userId);
+        User existingUser = validateUser(userId);
 
         // 유저 삭제
         userRepository.deleteById(userId);
@@ -84,5 +130,8 @@ public class UserService {
                 .orElseThrow(() -> new EntityNotFoundException("유저가 존재하지 않습니다."));
     }
 
+    // 비밀번호 해시 처리 메소드
+    public String hashPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
 }
-
